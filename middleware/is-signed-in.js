9b361f5341d8801bac71d4a26e45e76cbe0e75b3
Isaacj3000 <Isaacjeanbaptiste95@gmail.com>
2025-01-21 21:01:08 -0500

    make sure user can sign in

--- middleware/is-signed-in.js
+++ middleware/is-signed-in.js
@@ -1,7 +1,19 @@
+// const isSignedIn = (req, res, next) => {
+//     if (req.session.user) return next();
+//     res.redirect('/auth/sign-in');
+// };
+
+
+// module.exports = isSignedIn;
+
 const isSignedIn = (req, res, next) => {
-    if (req.session.user) return next();
+    console.log('Session data:', req.session);  // Log the whole session
+    if (req.session.user) {
+        console.log('User is signed in:', req.session.user);  // Log the user object
+        return next();
+    }
+    console.log('No user found, redirecting to sign-in.');
     res.redirect('/auth/sign-in');
 };
-
 
 module.exports = isSignedIn;
